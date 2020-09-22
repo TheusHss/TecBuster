dc412007fc39dc8f00992deb50aa4ffe6c712dde
TheusHss <matheus.soares@bandtec.com>
2020-09-22 18:55:21 -0300

    Criando o jogo

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function e1(){
+    ee2.style.display="block"
+    frase1.style.display="none"
+    frase2.style.display="block"
+frase3.style.display="none"
+frase4.style.display="none"
+};
+function e2(){
+    frase4.style.display="none"
+    ee3.style.display="block"
+    frase2.style.display="none"
+frase3.style.display="block"
+}
+function e3(){
+    ee2.style.display="none"
+    ee3.style.display="none"
+    ee1.style.display="none"
+    ee4.style.display="block"
+    ee5.style.display="block"
+    imgo.style.display="none"
+    img1.style.display="block"
+    frase4.style.display="block"
+    frase2.style.display="none"
+    frase3.style.display="none"
+}
+function e5(){
+    frase4.style.display="none"
+    img1.style.display="none"
+    ee5.style.display="none"
+    img2.style.display="block"
+    frase6.style.display="block"
+}
