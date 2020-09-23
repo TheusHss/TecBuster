a9588c9ec97b9ef172c4f607e486e840044a1f79
TheusHss <matheus.soares@bandtec.com>
2020-09-23 17:29:16 -0300

    add imagens nave e mundo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function e1(){
+    mundo_destruido.style.display='none'
+nave.style.display='block'
     ee2.style.display="block"
     frase1.style.display="none"
     frase2.style.display="block"
@@ -6,12 +8,16 @@ frase3.style.display="none"
 frase4.style.display="none"
 };
 function e2(){
+    mundo_destruido.style.display='block'
+    nave.style.display='none'
     frase4.style.display="none"
     ee3.style.display="block"
     frase2.style.display="none"
 frase3.style.display="block"
 }
 function e3(){
+    mundo_destruido.style.display='none'
+    nave.style.display='none'
     ee2.style.display="none"
     ee3.style.display="none"
     ee1.style.display="none"
