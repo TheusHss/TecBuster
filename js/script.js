f4e9adb64cc899096a2694e5b7391a3edaf322aa
TheusHss <matheus.soares@bandtec.com>
2020-09-24 22:12:11 -0300

    add segunda parte

--- js/script.js
+++ js/script.js
@@ -29,10 +29,63 @@ function e3(){
     frase2.style.display="none"
     frase3.style.display="none"
 }
+function e4(){
+    tudo1.style.display="none"
+    tudo2.style.display="block"
+}
 function e5(){
     frase4.style.display="none"
     img1.style.display="none"
     ee5.style.display="none"
     img2.style.display="block"
     frase6.style.display="block"
+}
+function f1(){
+    b3.style.display="block"
+    b2.style.display="none"
+    c1.style.display="block"
+    ns.style.display="none"
+    ashone.style.display="none"
+    fala1.style.display="none"
+    ashtwo.style.display="block"
+    fala2.style.display='block'
+}
+function f2(){
+    b4.style.display="block"
+    igor.style.display="block"
+    c1.style.display="none"
+    ashtwo.style.display="none"
+    c2.style.display="block"
+b3.style.display="none"
+fala2.style.display="none"
+fala3.style.display="block"
+}
+function f3(){
+    ashonea.style.display="block"
+    b4.style.display="none"
+    fala4.style.display="block"
+    c2.style.display="none"
+    fala3.style.display="none"
+    igor.style.display="none"
+    b5.style.display="block"
+    b6.style.display="block"
+    b7.style.display="block"
+}
+function f4(){
+    fala4.style.display="none"
+fala5.style.display="block"
+fala7.style.display="none"
+fala6.style.display="none"
+}
+function f5(){
+    fala4.style.display="none"
+    fala7.style.display="none"
+    fala5.style.display="none"
+    fala6.style.display="block"
+}
+function f6(){
+    fala4.style.display="none"
+    fala5.style.display="none"
+    fala6.style.display="none"
+    fala7.style.display="block"
 }
